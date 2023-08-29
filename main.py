14b8343f6642f742fe5e3ca46bafba64c9c7f83e
19388 <19388@LiuYulin>
2023-08-29 18:46:56 +0800

    sixth commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import sys
-
 import traci
+import xml.etree.ElementTree as ET
 
 sys.path.append('models/')
 from models import environment
@@ -22,8 +22,19 @@ if __name__ == '__main__':
     sumo_configuration()
     network_file = './environments_files/network_files/two_crossing_connected.net.xml'
     flow_file = './environments_files/flow_files/two_congested.rou.xml'
+    
+    tree = ET.parse(flow_file)
+    root = tree.getroot()
+    for vehicle_elem in root.findall('vehicle'):
+        vehicle_id = vehicle_elem.get('id')
+        vehicle_route = vehicle_elem.get('route')
+        depart_time = vehicle_elem.get('depart')
+        
+        print(f"Vehicle ID: {vehicle_id}")
+        print(f"Route ID: {vehicle_route}")
+        print(f"Depart Time: {depart_time}")
 
-    env = environment.traffic_env(network_file, flow_file)
+    
 
     sumoBinary = "C:/sumo-1.9.2/bin/sumo-gui"
     sumoCmd = [sumoBinary, "-c", './environments_files/first.sumocfg']
@@ -33,7 +44,15 @@ if __name__ == '__main__':
     step = 0
     while step < 1000:
         traci.simulationStep()
-        if traci.inductionloop.getLastStepVehicleNumber("0") > 0:
-            traci.trafficlight.setRedYellowGreenState("0", "GrGr")
-            step += 1
+        
+        vehicle_ids = traci.vehicle.getIDList()
+        for vehicle_id in vehicle_ids:
+            vehicle_lane = traci.vehicle.getLaneID(vehicle_id)
+            vehicle_speed = traci.vehicle.getSpeed(vehicle_id)
+            print(f"Vehicle ID: {vehicle_id}, Lane: {vehicle_lane}, Speed: {vehicle_speed}")
+
+        step += 1
+        
     traci.close()
+    
+    
