8e86bcf2aa53db21a3268ad18a57d5216c556b87
19388 <19388@LiuYulin>
2023-08-29 17:05:47 +0800

    third commit

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import sys
 import traci
 
 sys.path.append('models/')
-
+from models import environment
+from models import agent
 
 def sumo_configuration():
-    os.environ["SUMO_HOME"] = r"C:\sumo-1.9.2"
+    os.environ["SUMO_HOME"] = "C:/sumo-1.9.2"
 
     if 'SUMO_HOME' in os.environ:
         tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
@@ -20,17 +21,18 @@ if __name__ == '__main__':
     sumo_configuration()
     network_file = 'environments_files/network_files/two_crossing_connected.net.xml'
     flow_file = 'environments_files/flow_files/two_congested.rou.xml'
+    
+    env = environment.traffic_env(network_file, flow_file)
 
-sumoBinary = r"C:\sumo-1.9.2\bin\sumo-gui"
-sumoCmd = [sumoBinary, "-c", r"C:\Users\19388\Desktop\first\environments_files\first.sumocfg"]
+    sumoBinary = "C:/sumo-1.9.2/bin/sumo-gui"
+    sumoCmd = [sumoBinary, "-c", "C:/Users/19388/Desktop/first/environments_files/first.sumocfg"]
 
-traci.start(sumoCmd)
+    traci.start(sumoCmd)
 
-step = 0
-while step < 1000:
-    traci.simulationStep()
-    if traci.inductionloop.getLastStepVehicleNumber("0") > 0:
-        traci.trafficlight.setRedYellowGreenState("0", "GrGr")
-    step += 1
-
-traci.close()
+    step = 0
+    while step < 1000:
+        traci.simulationStep()
+        if traci.inductionloop.getLastStepVehicleNumber("0") > 0:
+            traci.trafficlight.setRedYellowGreenState("0", "GrGr")
+            step += 1
+    traci.close()
