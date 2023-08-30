cc9742caff5858dc6d1c913d24882c697a67e46b
19388 <19388@LiuYulin>
2023-08-30 21:01:55 +0800

    thirteenth commit

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import xml.etree.ElementTree as ET
 
 
 sys.path.append('models/')
-# from models import environment
-from models import agent
+from models.environment import StateSpace
+# from models import agent
 
 
 def sumo_configuration():
@@ -18,29 +18,9 @@ def sumo_configuration():
     else:
         sys.exit("please declare environment variable 'SUMO_HOME'")
 
-
-class StateSpace:
-    def __init__(self):
-        self.vehicles = {}  # 字典，存储车辆信息
-
-    def add_vehicle(self, vehicle_id, vehicle_lane, vehicle_speed):
-        self.vehicles[vehicle_id] = {
-            "lane": vehicle_lane,
-            "speed": vehicle_speed
-        }
-
-    def get_vehicle_info(self, vehicle_id):
-        return self.vehicles.get(vehicle_id, None)
-
-    def update_vehicle_info(self, vehicle_id, new_lane, new_speed):
-        if vehicle_id in self.vehicles:
-            self.vehicles[vehicle_id]["lane"] = new_lane
-            self.vehicles[vehicle_id]["speed"] = new_speed
-        else:
-            print("Vehicle not found in the state space.")
-
 # 创建状态空间对象
 state_space = StateSpace()
+initial_state_size = len(state_space.tls)
 
 if __name__ == '__main__':
     sumo_configuration()
@@ -57,15 +37,31 @@ if __name__ == '__main__':
         print(f"Vehicle ID: {vehicle_id}")
         print(f"Route ID: {vehicle_route}")
         print(f"Depart Time: {depart_time}")
-        
-        
     
+    
+    tree2 = ET.parse(network_file)
+    root2 = tree2.getroot()
+
+    for tl_logic in root2.findall('tlLogic'):
+        tl_id = tl_logic.get('id')
+        program_id = tl_logic.get('programID')
+    
+        print(f"Traffic Light ID: {tl_id}, Program ID: {program_id}")
+
+        for phase in tl_logic.findall('phase'):
+            phase_state = phase.get('state')
+        
+            ryg_state = state_space.phase_to_ryg(phase_state)  # 使用之前定义的函数将相位状态转换为 RYG 形式
+            print(f"Phase State (RYG) for Traffic Light {tl_id}: {ryg_state}")
 
     sumoBinary = "C:/sumo-1.9.2/bin/sumo-gui"
     sumoCmd = [sumoBinary, "-c", './environments_files/first.sumocfg']
 
     traci.start(sumoCmd)
-
+    
+    
+    
+        
     step = 0
     while step < 86:
         traci.simulationStep()
@@ -78,6 +74,23 @@ if __name__ == '__main__':
             state_space.add_vehicle(vehicle_id, vehicle_lane, vehicle_speed)
             print(f"state_space updated at step {step}: {vehicle_id, vehicle_lane,vehicle_speed}")
         
+        
+        
+        tl_ids = traci.trafficlight.getIDList()
+        for tl_id in tl_ids:
+            program_id = traci.trafficlight.getProgram(tl_id)
+            
+            # 获取交通信号灯当前相位状态
+            current_phase = traci.trafficlight.getRedYellowGreenState(tl_id)
+        
+        
+            # 将相位状态转换为 RYG 形式
+            ryg_phase = phase_to_ryg(current_phase)
+            
+            state_space.add_tl(tl_id, ryg_phase)
+            print(f"Traffic_Light updated at step {step}: {tl_id, ryg_phase}")
+       
+                  
         step += 1
         
     traci.close()   
